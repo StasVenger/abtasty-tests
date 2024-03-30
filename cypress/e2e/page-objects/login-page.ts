7d8acb8ac3d17c6d6ef8cdc3cec84ce2d8febd89
StasVenger <stasvenger1@gmail.com>
2024-03-31 03:11:04 +0600

    Added elements.

--- cypress/e2e/page-objects/login-page.ts
+++ cypress/e2e/page-objects/login-page.ts
@@ -3,6 +3,12 @@ interface LoginPageElements {
   password: string;
   signInButton: string;
   forgotPassword: string;
+  showPassword: string;
+  hidePassword: string;
+  pageTitle: string;
+  ssoLoginButton: string;
+  emailErrorMessage: string;
+  loginErrorMessage: string;
 }
 
 export default class LoginPage {
@@ -14,6 +20,12 @@ export default class LoginPage {
       password: '[data-testid=passwordInput] input#password',
       signInButton: '[#signInButton]',
       forgotPassword: '[data-testid=resetPasswordlink]',
+      showPassword: '[data-testid=showIcon]',
+      hidePassword: '[data-testid=hideIcon]',
+      pageTitle: 'h1',
+      ssoLoginButton: '[data-testid=ssoLoginButton]',
+      emailErrorMessage: '[data-testid=emailErrorMessage]',
+      loginErrorMessage: '[data-testid=loginErrorMessage]',
     };
   }
 
@@ -31,5 +43,29 @@ export default class LoginPage {
 
   async clickForgotPassword() {
     cy.get(this.elements.forgotPassword).click();
+  }
+
+  async clickShowPassword() {
+    cy.get(this.elements.showPassword).click();
+  }
+
+  async clickHidePassword() {
+    cy.get(this.elements.hidePassword).click();
+  }
+
+  async getPageTitle(title: string) {
+    cy.get(this.elements.pageTitle).should('have.text', title);
+  }
+
+  async clickSSOLoginButton() {
+    cy.get(this.elements.ssoLoginButton).click();
+  }
+
+  async getEmailErrorMessage(errorMessage: string) {
+    cy.get(this.elements.emailErrorMessage).should('have.text', errorMessage);
+  }
+
+  async getLoginErrorMessage(errorMessage: string) {
+    cy.get(this.elements.loginErrorMessage).should('have.text', errorMessage);
   }
 }
